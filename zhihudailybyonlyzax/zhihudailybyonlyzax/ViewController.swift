1a77da750eb3603dc5de3ff1b9e5df9fe541c253
onlyzax <zouyxo@outlook.com>
2017-07-11 22:50:41 +0800

    Create project
    Create porject and delete Main.storyboard

---
+++ zhihudailybyonlyzax/zhihudailybyonlyzax/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  zhihudailybyonlyzax
+//
+//  Created by Zouy on 2017/7/11.
+//  Copyright © 2017年 onlyzax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
